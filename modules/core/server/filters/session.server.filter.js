e9a637fff43d0bc755ccf5595dafb60bc0d6d034
Antony ZHANG <antony.zhang@cargosmart.com>
2017-11-11 23:31:51 +0800

    Adding the server filters.

---
+++ modules/core/server/filters/session.server.filter.js
@@ -0,0 +1,113 @@
+/**
+ * Created by Antony on 11/10/2017.
+ */
+
+var Util = require('../utils/core.server.util');
+var Config = require('../../../../config/config');
+
+var _ = require('lodash');
+
+var URLS_NOT_REQUIRE_LOGIN = [
+  '/',
+  '/user/signin',
+  '/user/signup',
+  '/user/logout'
+];
+
+var RESPONSE_TYPE_JSON = "JSON";
+var RESPONSE_JSON_CONTENT = { message: 'unauthorized' };
+var RESPONSE_TYPE_REDIRECT_INVALID_LOGIN_PAGE = 'REDIRECT_INVALID_LOGIN_PAGE';
+var INVALID_LOGIN_URL = '/#!/forbidden';
+var UNKNOWN_URL = '/#!/not-found';
+
+// the mapping like as : url -> { authCode: 'XXXX', responseType: 'JSON'}
+var URL_PERMISSION_MAPPING = {
+  '/api/admin/changepassword': { permissionType: 'FREE', authCode: 'USER_ADMIN', responseType: RESPONSE_TYPE_JSON },
+  '/api/admin/users/update': { permissionType: 'FREE', authCode: 'USER_ADMIN', responseType: RESPONSE_TYPE_JSON },
+  '/api/admin/users/profile': { permissionType: 'FREE', authCode: 'USER_ADMIN', responseType: RESPONSE_TYPE_JSON }
+};
+
+module.exports = function (req, res, next) {
+  var currentPath = req.path;
+
+  if (Util.isPublicResource(currentPath)) {
+    next();
+  } else {
+    if (isUrlNotRequireLogin(currentPath)) {
+      next();
+    } else {
+      Util.checkAndRefreshSession(req, res);
+      var permission = getPermission(currentPath);
+      if (permission) {
+        if (checkPermission(req, permission)) {
+          next();
+        } else if (permission.responseType === RESPONSE_TYPE_JSON) {
+          res.status(403).send(RESPONSE_JSON_CONTENT);
+        } else {
+          res.redirect(INVALID_LOGIN_URL);
+        }
+      } else {
+        if(_.startsWith(currentPath, '/api')
+          || _.startsWith(currentPath, '/client_modules')
+          || _.startsWith(currentPath, '/core')
+          || _.startsWith(currentPath, '/favicon.ico')
+          || _.startsWith(currentPath, '/#!')
+        ) {
+          res.redirect(UNKNOWN_URL);
+        } else {
+          res.redirect('/#!' + currentPath);
+        }
+      }
+    }
+  }
+};
+
+var checkPermission = function (req, permission) {
+  if (!req.isAuthenticated()) {
+    return false;
+  }
+
+  if (permission.permissionType == 'FREE') {
+    return req.user.userType == 'PAID' || req.user.userType == 'FREE';
+  }
+
+  if (permission.permissionType == 'PAID') {
+    // return Util.hasPermission(req.user.paidProducts, permission.authCode);
+    return true;
+  }
+
+  return false;
+};
+
+var getPermission = function (currentPath) {
+  if (URL_PERMISSION_MAPPING[currentPath]) {
+    return URL_PERMISSION_MAPPING[currentPath];
+  }
+  for (var url in URL_PERMISSION_MAPPING) {
+    var pathRegex = "^" + url.replace(/\*/g, '(.*)') + '$';
+    var pattern = new RegExp(pathRegex, "i");
+    if (pattern.test(currentPath)) {
+      return URL_PERMISSION_MAPPING[url];
+    }
+  }
+
+  return null;
+};
+
+var isUrlNotRequireLogin = function (currentPath) {
+  for (var i = 0; i < URLS_NOT_REQUIRE_LOGIN.length; i++) {
+    var pathRegex = "^" + URLS_NOT_REQUIRE_LOGIN[i].replace(/\*/g, '(.*)') + '$';
+    var pattern = new RegExp(pathRegex, "i");
+    if (pattern.test(currentPath)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+if (typeof String.prototype.endsWith !== 'function') {
+  String.prototype.endsWith = function (suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+  };
+}
---
+++ modules/core/server/utils/core.server.util.js
@@ -0,0 +1,65 @@
+/**
+ * Created by ZHANGAN2 on 11/10/2017.
+ */
+
+var Config = require('../../../../config/config');
+
+var PUBLIC_RESOURCE_FILE_TYPES = [
+  '.htm', '.html',                                // static html page
+  '.js',                                          // javascript file
+  '.css',                                         // style
+  '.png', '.jpg', '.gif',                         // image
+  '.svg', '.eot', '.woff', '.woff2', '.ttf',      // font
+  '.pdf', '.json'                                          // PDF file
+];
+
+var SESSION_ID_COOKIE_KEY = 'MEAN_SCAFFOLD_ID';
+var SESSION_ID_EXPIRATION_AGE = 3 * 60 * 60 * 1000; // 3 hours.
+
+var isPublicResource = function(currentPath) {
+  for (var i = 0; i < PUBLIC_RESOURCE_FILE_TYPES.length; i++) {
+    var fileType = PUBLIC_RESOURCE_FILE_TYPES[i];
+    if (currentPath.endsWith(fileType)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+var getSessionId = function (req) {
+  return req.cookies[SESSION_ID_COOKIE_KEY];
+};
+
+var clearSessionId = function(res) {
+  if (!Config.sessionCookie.maxAge) {
+    res.cookie(SESSION_ID_COOKIE_KEY, null, { maxAge: 0 });
+  }
+};
+
+var resetSessionIdExpiredTime = function(req, res, meanScaffoldId) {
+  if (!Config.sessionCookie.maxAge && meanScaffoldId && meanScaffoldId != '') {
+    res.cookie(SESSION_ID_COOKIE_KEY, meanScaffoldId, { maxAge: SESSION_ID_EXPIRATION_AGE });
+  }
+};
+
+var checkAndRefreshSession = function (req, res) {
+  if (Config.sessionCookie.maxAge) {
+    res.cookie(Config.sessionKey, req.cookies[Config.sessionKey], { maxAge: Config.sessionCookie.maxAge });
+    return;
+  }
+
+  if (req.user) {
+    if (!req.user.sessionId || req.user.sessionId != getSessionId(req)) {
+      clearSessionId(res);
+      req.logout();
+    } else {
+      resetSessionIdExpiredTime(req, res, req.user.sessionId);
+    }
+  } else if (getSessionId(req)) {
+    clearSessionId(res);
+  }
+};
+
+exports.isPublicResource = isPublicResource;
+exports.checkAndRefreshSession = checkAndRefreshSession;
