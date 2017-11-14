52949bcc6c63d2121818591d50b5932c22e805bf
Antony ZHANG <antony.zhang@cargosmart.com>
2017-11-14 15:27:48 +0800

    Remove the policies and acl module.

--- modules/articles/server/routes/articles.server.routes.js
+++ modules/articles/server/routes/articles.server.routes.js
@@ -3,17 +3,16 @@
 /**
  * Module dependencies
  */
-var articlesPolicy = require('../policies/articles.server.policy'),
-  articles = require('../controllers/articles.server.controller');
+var articles = require('../controllers/articles.server.controller');
 
 module.exports = function (app) {
   // Articles collection routes
-  app.route('/api/articles').all(articlesPolicy.isAllowed)
+  app.route('/api/articles')
     .get(articles.list)
     .post(articles.create);
 
   // Single article routes
-  app.route('/api/articles/:articleId').all(articlesPolicy.isAllowed)
+  app.route('/api/articles/:articleId')
     .get(articles.read)
     .put(articles.update)
     .delete(articles.delete);
